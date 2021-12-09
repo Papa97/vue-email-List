6e9be12879330625febae29322bfc17e4b3b9eb7
Guido <ppapaguido@gmail.com>
2021-12-09 16:10:51 +0100

    prima mailstone, si vedono tutte e 10 le mail

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const app = new Vue ({
+    el:'#root',
+    data:{
+        mails : [],
+    },
+    methods: {
+
+    },
+    created() {
+
+        for (let i = 0 ; i < 10 ; i ++ ){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+            this.mails.push(response.data.response)
+        })
+        }
+
+        
+    }
+});
